5f1235486c1e0e76c055d2ea097f4b2a38610c0d
AmandaSanchez21 <asg_97@hotmail.com>
2017-11-27 19:47:18 +0100

    iteration 1

---
+++ ISO II-Spotif/src/Server.java
@@ -0,0 +1,57 @@
+public class Server {
+
+	public void addSong() {
+		// TODO - implement SERVER.addSong
+		throw new UnsupportedOperationException();
+	}
+
+	public void addAlbum() {
+		// TODO - implement SERVER.addAlbum
+		throw new UnsupportedOperationException();
+	}
+
+	public void modifySong() {
+		// TODO - implement SERVER.modifySong
+		throw new UnsupportedOperationException();
+	}
+
+	public void deleteSong() {
+		// TODO - implement SERVER.deleteSong
+		throw new UnsupportedOperationException();
+	}
+
+	public void deleteAlbum() {
+		// TODO - implement SERVER.deleteAlbum
+		throw new UnsupportedOperationException();
+	}
+
+	public Song searchSong() {
+		// TODO - implement SERVER.searchSong
+		throw new UnsupportedOperationException();
+	}
+
+	public void createUserManually() {
+		// TODO - implement SERVER.createUserManually
+		throw new UnsupportedOperationException();
+	}
+
+	public void createUserAutomatically() {
+		// TODO - implement SERVER.createUserAutomatically
+		throw new UnsupportedOperationException();
+	}
+
+	public void deleteUser() {
+		// TODO - implement SERVER.deleteUser
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param user
+	 */
+	public void sendMessage(User user) {
+		// TODO - implement SERVER.sendMessage
+		throw new UnsupportedOperationException();
+	}
+
+}

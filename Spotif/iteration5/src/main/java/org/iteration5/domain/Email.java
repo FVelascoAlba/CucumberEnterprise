cc45895e1fe1fb1142e17766b1e00bb8c662ab13
AmandaSanchez21 <asg_97@hotmail.com>
2017-12-07 17:56:27 +0100

    Iteration 5 - Use Case 2

---
+++ Spotif/iteration5/src/main/java/org/iteration5/domain/Email.java
@@ -0,0 +1,36 @@
+public class EMAIL {
+
+	private String source;
+	private String destination;
+	private String content;
+
+	public string getSource() {
+		// TODO - implement EMAIL.getSource
+		throw new UnsupportedOperationException();
+	}
+
+	public String getDestination() {
+		return this.destination;
+	}
+
+	/**
+	 * 
+	 * @param dest
+	 */
+	public void setDestination(String dest) {
+		this.destination = dest;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	/**
+	 * 
+	 * @param cont
+	 */
+	public void setContent(String cont) {
+		this.content = cont;
+	}
+
+}

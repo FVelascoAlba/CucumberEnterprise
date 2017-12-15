f8e72421cbce8c698a3880670a739f9f5bdb04ea
moinickcres <moinickcres@gmail.com>
2017-12-15 21:43:35 +0100

    Iteration 8 - Use cases 6 & 11

--- Spotif/iteration8/src/main/java/org/iteration8/domain/Administrator.java
+++ Spotif/iteration8/src/main/java/org/iteration8/domain/Administrator.java
@@ -1,3 +1,12 @@
-package org.iteration2.domain;
+package Domain;
+
+import java.util.ArrayList;
+
 public class Administrator extends User {
+
+	public Administrator(String name, String surname, String userName, String password, ArrayList<Song> songs_bought,
+			ArrayList<Album> albums_bought, ArrayList<ArrayList<Song>> list_songs) {
+		super(name, surname, userName, password, songs_bought, albums_bought, list_songs);
+	}
+
 }

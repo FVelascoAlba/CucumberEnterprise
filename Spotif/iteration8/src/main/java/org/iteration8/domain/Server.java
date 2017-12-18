c1287fce0aa5ed27a7d7d605e48da9cc0b8e698d
Velasco <fernandovalba@hotmail.com>
2017-12-18 19:42:18 +0100

    v1.8.2

--- Spotif/iteration8/src/main/java/org/iteration8/domain/Server.java
+++ Spotif/iteration8/src/main/java/org/iteration8/domain/Server.java
@@ -23,5 +23,21 @@ public class Server {
 	public void deleteUser(User u) {}
 	
 	public void sendMessage(User u) {}
-
+	
+	public void purchaseAlbum (String name, String released_date, String artist, int price) {
+		Album al = new Album();
+		if (name.equals("") && released_date.equals("") && artist.equals("") && price == 0) {
+			if(name.length() < 255 && relased_date.length() < 255 && artist.length() < 255 && price < 50) {
+				if(payProduct(al)) {
+					
+				}
+			}
+		}
+		
+	}
+	
+	public boolean payProduct(Album al) {
+		
+	}
 }
+
